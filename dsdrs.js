071e665ca83f4c3065198fa3594f3251df2f219b
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-07-29 00:50:09 -0500

    Update dsdrs.js

--- dsdrs.js
+++ dsdrs.js
@@ -29,56 +29,40 @@
 
     runScript();
 
-    function runScript() {
-        function f(selector, value, delay) {
-            setTimeout(function () {
-                var e = document.querySelector(selector);
+    function fill(selector, value) {
+        var e = document.querySelector(selector);
 
-                if (e) {
-                    e.focus();
-                    e.value = value;
-
-                    e.dispatchEvent(new Event("input", { bubbles: true }));
-                    e.dispatchEvent(new Event("change", { bubbles: true }));
-
-                    setTimeout(function () {
-                        e.dispatchEvent(new KeyboardEvent("keydown", {
-                            key: "Enter",
-                            keyCode: 13,
-                            which: 13,
-                            bubbles: true
-                        }));
-
-                        e.dispatchEvent(new KeyboardEvent("keypress", {
-                            key: "Enter",
-                            keyCode: 13,
-                            which: 13,
-                            bubbles: true
-                        }));
-
-                        e.dispatchEvent(new KeyboardEvent("keyup", {
-                            key: "Enter",
-                            keyCode: 13,
-                            which: 13,
-                            bubbles: true
-                        }));
-                    }, 100);
-                } else {
-                    alert("Element not found: " + value);
-                }
-            }, delay);
+        if (!e) {
+            alert("Element not found:\n" + selector);
+            return false;
         }
 
-        f(
-            "#ngForm > fieldset > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > ng-select > div > div > div.ng-input > input[type=text]",
-            "Airrish Dullas",
-            0
-        );
+        e.focus();
+        e.value = value;
 
-        f(
-            "#ngForm > fieldset > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > ng-select > div > div > div.ng-input > input[type=text]",
-            "VOB verified, no change to NSA jurisdiction",
-            0
-        );
+        e.dispatchEvent(new Event("input", { bubbles: true }));
+        e.dispatchEvent(new KeyboardEvent("keydown", {
+            key: "Enter",
+            code: "Enter",
+            keyCode: 13,
+            which: 13,
+            bubbles: true
+        }));
+
+        return true;
+    }
+
+    function runScript() {
+        var owner =
+            "#ngForm > fieldset > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > ng-select > div > div > div.ng-input > input[type=text]";
+
+        var note =
+            "#ngForm > fieldset > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > ng-select > div > div > div.ng-input > input[type=text]";
+
+        fill(owner, "Airrish Dullas");
+
+        setTimeout(function () {
+            fill(note, "VOB verified, no change to NSA jurisdiction");
+        }, 20);
     }
 })();
