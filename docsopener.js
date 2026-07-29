f8b6cd26d9191c9a2df84b469adbf9fdc709300a
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-07-29 00:33:08 -0500

    Update docsopener.js

--- docsopener.js
+++ docsopener.js
@@ -1,13 +1,40 @@
 (function () {
-    var s = document.createElement('script');
-
-    s.src =
-        'https://buttons.airrishlucky-dullas.workers.dev/docsopener?v=' +
-        Date.now();
-
-    s.onerror = function () {
-        alert('Failed to load Docs Opener');
-    };
-
-    document.head.appendChild(s);
+    var selectors = [
+        "#ngForm > fieldset > div:nth-child(22) > div.d-flex.mb-2 > button",
+        "#ngForm > fieldset > div:nth-child(24) > div.d-flex.mb-2 > button"
+    ];
+ 
+    selectors.forEach(function (selector) {
+        var el = document.querySelector(selector);
+ 
+        if (el) {
+            el.click();
+ 
+            el.style.outline = "3px solid orange";
+ 
+            setTimeout(function () {
+                el.style.outline = "";
+            }, 3000);
+        }
+    });
+ 
+    setTimeout(function () {
+        var target = document.querySelector(
+            "body > app-root > div > div.container-fluid.ps-0.pe-0.h-100.pb-4 > app-calculator > div > div.card-body > div:nth-child(3) > div.d-flex.mb-2.align-items-center"
+        );
+ 
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+ 
+            target.style.outline = "3px solid red";
+ 
+            setTimeout(function () {
+                target.style.outline = "";
+            }, 3000);
+        }
+    }, 300);
 })();
+ 
