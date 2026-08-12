0ef4dd8a23f9965db60461b6ffb452e24caacf9f
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-08-12 18:36:40 -0500

    Update singleappid.js

--- singleappid.js
+++ singleappid.js
@@ -1,13 +1,13 @@
 (function () {
-
     const el = document.querySelector(
         '#table-body > tr > td:nth-child(2) > a'
     );
 
     if (el) {
+        el.target = '_self';   // open in current tab
+        el.removeAttribute('target');
         el.click();
     } else {
         alert('Element not found');
     }
-
 })();
