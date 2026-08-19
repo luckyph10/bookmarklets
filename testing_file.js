c90975b577b38406533a6ca22681c3b9a8c4eefe
luckyph10 <airrishlucky.dullas@globalstrategic.com>
2026-08-18 19:39:20 -0500

    Update testing_file.js

--- testing_file.js
+++ testing_file.js
@@ -476,6 +476,22 @@ const popup=()=>new Promise(resolve=>{
         }
     };
 
+    overlay.onkeydown=e=>{
+    if(e.key==="Escape"){
+        overlay.remove();
+        style.remove();
+        resolve(null);
+    }
+
+    if(
+        e.key==="0" &&
+        eligible.style.display==="block"
+    ){
+        e.preventDefault();
+        noBtn.click();
+    }
+};
+
     stateInput.focus();
 });
 
